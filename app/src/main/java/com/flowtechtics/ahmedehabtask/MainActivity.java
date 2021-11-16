e0a97c67576260491617a2d96c0e70c7e153d1b3
Ahmed Ehab <ehab@Ahmeds-MacBook-Pro.local>
2021-11-16 16:43:56 +0200

    compeleted task

--- app/src/main/java/com/flowtechtics/ahmedehabtask/MainActivity.java
+++ app/src/main/java/com/flowtechtics/ahmedehabtask/MainActivity.java
@@ -2,7 +2,6 @@ package com.flowtechtics.ahmedehabtask;
 
 import static com.flowtechtics.ahmedehabtask.utilities.ImageHelper.getBitmapFromURL;
 
-import static org.testng.AssertJUnit.assertEquals;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,27 +13,17 @@ import android.util.Log;
 import android.view.View;
 
 
-import com.ehab.networkFramework.JSONObjectRequestListener;
-import com.ehab.networkFramework.NetworkConnect;
+import com.bumptech.glide.Glide;
+
 import com.flowtechtics.ahmedehabtask.databinding.ActivityMainBinding;
 import com.flowtechtics.ahmedehabtask.models.DataModel;
 import com.flowtechtics.ahmedehabtask.presenter.HomePresenter;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.json.JSONTokener;
-import org.testng.annotations.Test;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-
-public class MainActivity extends AppCompatActivity implements DataListener{
+public class MainActivity extends AppCompatActivity implements DataListener {
     private static final String JSON_URL = "https://api.github.com/users/brynary";
-    private ActivityMainBinding binding ;
-    HomePresenter presenter ;
+    private ActivityMainBinding binding;
+    HomePresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,9 +35,9 @@ public class MainActivity extends AppCompatActivity implements DataListener{
 
     }
 
-    private void init(){
+    private void init() {
         presenter = new HomePresenter(this);
-        presenter.getDataFromUrl(JSON_URL , "GET");
+        presenter.getDataFromUrl(JSON_URL, "GET");
 
     }
 
@@ -57,13 +46,20 @@ public class MainActivity extends AppCompatActivity implements DataListener{
         setUserData(dataModel);
     }
 
-    private void setUserData(DataModel model){
+    private void setUserData(DataModel model) {
         binding.profilePic.setClipToOutline(true);
         binding.name.setText(model.getName());
         binding.companyName.setText(model.getCompany());
         binding.bio.setText(model.getBio());
         binding.type.setText(model.getType());
+        loadImage(model.getAvatar_url());
 
+    }
+
+    private void loadImage(String url) {
+        Glide.with(this)
+                .load(url)
+                .into(binding.profilePic);
     }
 
 }
